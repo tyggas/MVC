bfd0aa65129bbf091debe63f2253d2b9607e992a
tyggas <tagrigull@gmail.com>
2024-12-11 20:58:39 -0300

    editar dados

--- MVC/Controllers/CarrosController.cs
+++ MVC/Controllers/CarrosController.cs
@@ -5,7 +5,7 @@ namespace MVC.Controllers
 {
     public class CarrosController : Controller
     {
-        public List<CarroModel> _Carros = new List<CarroModel>()
+        static List<CarroModel> _Carros = new List<CarroModel>()
         {   new CarroModel(){id =1, Nome="corsa", Ano= 2000, Placa="abc123" },
             new CarroModel(){id =2, Nome="corsa", Ano= 2000, Placa="abc123" },
             new CarroModel(){id =3, Nome="corsa", Ano= 2000, Placa="abc123" },
@@ -22,6 +22,15 @@ namespace MVC.Controllers
             var carroAtual = _Carros.FirstOrDefault(carro => carro.id == id);
 
             return View(carroAtual);
+        }
+        [HttpPost]
+        public IActionResult Salvar(CarroModel model)
+        {
+            var carros = _Carros.FirstOrDefault(i => i.id == model.id);
+            carros.Nome = model.Nome;
+            carros.Ano = model.Ano;
+            carros.Placa = model.Placa;
+            return RedirectToAction("Index");
         }
 
     }
--- MVC/Controllers/FuncionariosController.cs
+++ MVC/Controllers/FuncionariosController.cs
@@ -7,10 +7,10 @@ namespace MVC.Controllers
     {
         public List<FuncionarioModel> _Funcionario = new List<FuncionarioModel>()
         {
-            new FuncionarioModel() { Id = 1, Nome = "Pedro", Cargo = "gerente", Salário = 1000.00 },
-            new FuncionarioModel() { Id = 2, Nome = "Pedro", Cargo = "gerente", Salário = 1000.00 },
-            new FuncionarioModel() { Id = 3, Nome = "Pedro", Cargo = "gerente", Salário = 1000.00 },
-            new FuncionarioModel() { Id = 4, Nome = "Pedro", Cargo = "gerente", Salário = 1000.00 }
+            new FuncionarioModel() { Id = 1, Nome = "Tiago", Cargo = "gerente", Salário = 1000.00 },
+            new FuncionarioModel() { Id = 2, Nome = "Tiago", Cargo = "gerente", Salário = 1000.00 },
+            new FuncionarioModel() { Id = 3, Nome = "Tiago", Cargo = "gerente", Salário = 1000.00 },
+            new FuncionarioModel() { Id = 4, Nome = "Tiago", Cargo = "gerente", Salário = 1000.00 }
         };
         public IActionResult Index()
         {
@@ -23,6 +23,16 @@ namespace MVC.Controllers
             var funcionarioAtual = _Funcionario.FirstOrDefault(funcionario => funcionario.Id == id);
 
             return View(funcionarioAtual);
+        }
+
+        [HttpPost]
+        public IActionResult Salvar(FuncionarioModel model)
+        {
+            var func = _Funcionario.FirstOrDefault(i => i.Id == model.Id);
+            func.Nome = model.Nome;
+            func.Cargo = model.Cargo;
+            func.Salário = model.Salário;
+            return RedirectToAction("Index");
         }
     }
 }
